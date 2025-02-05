007c009633ab67c63d147b306d97e9a797e131d6
Oleg <pasechnik.o.n@gmail.com>
2025-02-05 09:08:57 +0200

    - Fixed Unit Test for Driver Update Request Validator

--- source/domain/driver/core/src/test/java/ee/qrental/driver/core/validator/DriverUpdateRequestValidatorTest.java
+++ source/domain/driver/core/src/test/java/ee/qrental/driver/core/validator/DriverUpdateRequestValidatorTest.java
@@ -10,12 +10,15 @@ import ee.qrent.common.in.validation.AttributeChecker;
 import ee.qrental.common.core.validation.AttributeCheckerImpl;
 import ee.qrental.constant.api.in.query.GetQWeekQuery;
 import ee.qrental.constant.api.in.response.qweek.QWeekResponse;
+import ee.qrental.driver.api.in.request.DriverAddRequest;
 import ee.qrental.driver.api.in.request.DriverUpdateRequest;
 import ee.qrental.driver.api.out.DriverLoadPort;
 import ee.qrental.driver.domain.Driver;
 import ee.qrental.driver.domain.Friendship;
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -35,14 +38,42 @@ class DriverUpdateRequestValidatorTest {
     attributeChecker = new AttributeCheckerImpl();
     instanceUnderTest =
         new DriverUpdateRequestValidator(attributeChecker, loadPort, qWeekQuery, qDateTime);
+
+    when(qDateTime.getToday()).thenReturn(LocalDate.of(2025, Month.JANUARY, 15));
+  }
+
+  private DriverUpdateRequest getValidDriverUpdateRequest() {
+    final var updateRequest = new DriverUpdateRequest();
+    updateRequest.setId(5L);
+    updateRequest.setActive(true);
+    updateRequest.setHasRequiredObligation(Boolean.FALSE);
+    updateRequest.setFirstName("John");
+    updateRequest.setLastName("Doe");
+    updateRequest.setTaxNumber(48108259011L);
+    updateRequest.setAddress("Tallinn, Str. Lootsa, 45b");
+    updateRequest.setDriverLicenseNumber("AR-TY_45879");
+    updateRequest.setDriverLicenseExp(qDateTime.getToday().plus(2, ChronoUnit.DAYS));
+    updateRequest.setTaxiLicense("TLL-T-958");
+    updateRequest.setPhone("+3729876587, +383050478955");
+    updateRequest.setEmail("test.f.l@gmail.com");
+    updateRequest.setCompanyName("Driver&Company");
+    updateRequest.setRegNumber("REG-7878");
+    updateRequest.setCompanyAddress("Tallinn, Str. Lootsa, 88c");
+    updateRequest.setCompanyVat("VAT-8989");
+    updateRequest.setCompanyCeoFirstName("CEO_FN");
+    updateRequest.setCompanyCeoLastName("CEO_LN");
+    updateRequest.setCompanyCeoTaxNumber(48108259011L);
+    updateRequest.setComment("This is a comment");
+
+    return updateRequest;
   }
 
   @Test
   public void testIfDriverDidNotHaveRecommendationAndNoNewRecommendationInRequest() {
     // given
-    final var driverIdToUpdate = 5L;
-    final var updateRequest =
-        DriverUpdateRequest.builder().id(driverIdToUpdate).recommendedByDriverId(null).build();
+    final var updateRequest = getValidDriverUpdateRequest();
+    final var driverIdToUpdate = updateRequest.getId();
+    updateRequest.setRecommendedByDriverId(null);
     when(loadPort.loadById(driverIdToUpdate))
         .thenReturn(Driver.builder().id(driverIdToUpdate).friendship(null).build());
 
@@ -56,9 +87,9 @@ class DriverUpdateRequestValidatorTest {
   @Test
   public void testIfDriverHasRecommendationAndSameRecommendationInRequest() {
     // given
-    final var driverIdToUpdate = 5L;
-    final var updateRequest =
-        DriverUpdateRequest.builder().id(driverIdToUpdate).recommendedByDriverId(44L).build();
+    final var updateRequest = getValidDriverUpdateRequest();
+    updateRequest.setRecommendedByDriverId(44L);
+    final var driverIdToUpdate = updateRequest.getId();
     when(loadPort.loadById(driverIdToUpdate))
         .thenReturn(
             Driver.builder()
@@ -76,15 +107,10 @@ class DriverUpdateRequestValidatorTest {
   @Test
   public void testIfDriverHasRecommendationAndNewRecommendationInRequestAndWeekIsNotPassed() {
     // given
-    final var driverIdToUpdate = 5L;
-    final var requestRecommendedByDriverId = 44L;
+    final var updateRequest = getValidDriverUpdateRequest();
+    updateRequest.setRecommendedByDriverId(44L);
+    final var driverIdToUpdate = updateRequest.getId();
     final var dbRecommendedByDriverId = 33L;
-    final var updateRequest =
-        DriverUpdateRequest.builder()
-            .id(driverIdToUpdate)
-            .recommendedByDriverId(requestRecommendedByDriverId)
-            .build();
-
     when(loadPort.loadById(driverIdToUpdate))
         .thenReturn(
             Driver.builder()
@@ -116,15 +142,11 @@ class DriverUpdateRequestValidatorTest {
   @Test
   public void testIfDriverHasRecommendationAndNewRecommendationInRequestAndWeekIsPassed() {
     // given
-    final var driverIdToUpdate = 5L;
     final var requestRecommendedByDriverId = 44L;
     final var dbRecommendedByDriverId = 33L;
-    final var updateRequest =
-        DriverUpdateRequest.builder()
-            .id(driverIdToUpdate)
-            .recommendedByDriverId(requestRecommendedByDriverId)
-            .build();
-
+    final var updateRequest = getValidDriverUpdateRequest();
+    updateRequest.setRecommendedByDriverId(requestRecommendedByDriverId);
+    final var driverIdToUpdate = updateRequest.getId();
     when(loadPort.loadById(driverIdToUpdate))
         .thenReturn(
             Driver.builder()
