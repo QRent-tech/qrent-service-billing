5c8b064ee71cebb14ff3ec137ec67e973f91ce29
Oleg Pasichnyk <pasechnik.o.n@gmail.com>
2025-03-18 14:24:08 +0200

    Test Commit

--- source/app/src/main/java/ee/qrental/Application.java
+++ source/app/src/main/java/ee/qrental/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+  //TODO remove this comment
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
